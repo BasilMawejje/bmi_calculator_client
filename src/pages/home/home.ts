73733a4aade2645c293337b5c2d96a92a6cabc12
basil <basil.mawejje@gmail.com>
2017-10-26 11:19:33 +0200

    Add logic to client using providers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PersonProvider } from '../../providers/person/person';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  user: any = {};
 
+  constructor(
+    public navCtrl: NavController,
+    public person: PersonProvider
+  ) {
+    this.user = { distance: 1000, age: 20, gender: 'female' };
   }
 
+  calculate() {
+    this.person.age = this.user.age;
+    this.person.gender = this.user.gender;
+
+    this.person.doAssessment(this.user.distance);
+    console.log(this.person.assessmentMessage);
+  }
 }
